cabafab1e7af8ed30c344e144ba9ac0de178a1d4
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-06-23 02:43:57 +0530

    api bug fix

--- pokemon-adoption-app/src/api/index.js
+++ pokemon-adoption-app/src/api/index.js
@@ -1,7 +1,11 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:7000" });
+const API = axios.create({
+  baseURL:
+    "https://vercel.com/kaustubhpathak-tech/hyathi-fullstack-task-server",
+}); //http://localhost:7000
 export const signUp = (authData) => API.post("/user/signup", authData);
 export const logIn = (authData) => API.post("/user/login", authData);
 export const fetchAllPokemons = () => API.get("/pokemon/getAllPokemons");
-export const adoptPokemon = (userData) => API.patch("pokemon/adoptPokemon", userData);
-export const feedPokemon = (Data) => API.patch("pokemon/feedPokemon",Data);
+export const adoptPokemon = (userData) =>
+  API.patch("pokemon/adoptPokemon", userData);
+export const feedPokemon = (Data) => API.patch("pokemon/feedPokemon", Data);
